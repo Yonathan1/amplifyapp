d0a945648bda8e49c50595f98b4ce62f6f42471a
Yo <nathan.gunawan93@gmail.com>
2022-02-10 15:38:16 +0700

    Fix error in isPassedToWithAuthenticator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
 import React from 'react';
 import { Amplify } from 'aws-amplify';
+
+// import { Authenticator } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({isPassedToWithAuthenticator, signOut, user}) {
+function App({ isPassedToWithAuthenticator, signOut, user }) {
   if (!isPassedToWithAuthenticator) {
-    throw new Error(`isPassedToWithAuthenticator was not provided`);
+    throw new Error('isPassedToWithAuthenticator was not provided');
   }
+
   return (
     <>
       <h1>Hello {user.username}</h1>
@@ -22,9 +24,23 @@ function App({isPassedToWithAuthenticator, signOut, user}) {
 export default withAuthenticator(App);
 
 export async function getStaticProps() {
+  console.log("getstaticprops");
   return {
     props: {
       isPassedToWithAuthenticator: true,
     },
   };
 }
+
+// export default function App() {
+//   return (
+//     <Authenticator>
+//       {({ signOut, user }) => (
+//         <>
+//           <h1>Hello {user.username}</h1>
+//           <button onClick={signOut}>Sign out</button>
+//         </>
+//       )}
+//     </Authenticator>
+//   );
+// }
