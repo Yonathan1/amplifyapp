ab22c80847b85b0b30191f725ddbd2e3c3d97a14
Yo <nathan.gunawan93@gmail.com>
2022-02-10 15:47:37 +0700

    Use Authenticator Directly

--- src/App.js
+++ src/App.js
@@ -1,46 +1,21 @@
 import React from 'react';
 import { Amplify } from 'aws-amplify';
 
-// import { Authenticator } from '@aws-amplify/ui-react';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-function App({ isPassedToWithAuthenticator, signOut, user }) {
-  if (!isPassedToWithAuthenticator) {
-    throw new Error('isPassedToWithAuthenticator was not provided');
-  }
-
+export default function App() {
   return (
-    <>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign Out</button>
-    </>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <>
+          <h1>Hello {user.username}</h1>
+          <button onClick={signOut}>Sign out</button>
+        </>
+      )}
+    </Authenticator>
   );
 }
-
-export default withAuthenticator(App);
-
-export async function getStaticProps() {
-  console.log("getstaticprops");
-  return {
-    props: {
-      isPassedToWithAuthenticator: true,
-    },
-  };
-}
-
-// export default function App() {
-//   return (
-//     <Authenticator>
-//       {({ signOut, user }) => (
-//         <>
-//           <h1>Hello {user.username}</h1>
-//           <button onClick={signOut}>Sign out</button>
-//         </>
-//       )}
-//     </Authenticator>
-//   );
-// }
